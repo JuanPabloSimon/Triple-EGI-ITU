3bb3df23a87f4ab469b63cdd7eb12c3babac0210
Juan Pablo Simon <jsimon0301@gmail.com>
2026-06-03 19:10:31 -0300

    Add: Estructura de Front creada - Auth aplicado

---
+++ app/inventario-itu-client/src/App.tsx
@@ -0,0 +1,24 @@
+// src/App.tsx
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import DetallePage from "./pages/DetallePage";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/equipo/:id" element={<DetallePage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
